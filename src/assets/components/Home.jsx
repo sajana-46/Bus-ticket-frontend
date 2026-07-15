da34d27dc2c65e12cee30edf28f558974acb58c1
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-15 12:17:22 +0530

    initated react setups

---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <center>
+        <h1>Welcome To Bus-Ticket App</h1>
+        </center>
+    </div>
+  )
+}
+
+export default Home
