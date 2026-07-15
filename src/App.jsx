02b35bd1a76fb81fd06c5c040d5d1a12552d7bfa
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-15 12:42:56 +0530

    addbus completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './assets/components/Home'
+import Addbus from './components/Addbus'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <Home/>
+    <Addbus/>
       
     </>
   )
