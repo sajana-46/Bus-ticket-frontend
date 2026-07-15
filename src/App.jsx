c2c05c2fb87fa65fad85792ec61addf46fd21b12
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-15 13:02:02 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import './App.css'
 import Home from './assets/components/Home'
 import Addbus from './components/Addbus'
 import Viewbus from './components/Viewbus'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
-    <Addbus/>
-    <Viewbus/>
+     <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Addbus/>} />
+    <Route path='/view' element={<Viewbus/>} />
+        
+    </Routes>
+    </BrowserRouter>
       
     </>
   )
